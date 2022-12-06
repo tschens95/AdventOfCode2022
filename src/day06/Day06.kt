61a865d081a527a15a794e50749db8b5190b64ee
jheinen <jens.heinen@n4.de>
2022-12-06 09:44:28 +0100

    day06 refactor

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -4,42 +4,31 @@ import readText
 
 fun main() {
 
-    fun part1(input: String): Int {
-        var fourMarker = ""
-        var count = 0
-        for (c in input) {
-            if (c in fourMarker) {
-                val index = fourMarker.indexOf(c)
-                fourMarker = fourMarker.substring(index+1, fourMarker.length) + c
-            } else {
-                fourMarker += c
-            }
-            count++
-            if (fourMarker.length == 4) {
-                break
-            }
-        }
-        println(fourMarker)
-        return count
-    }
-
-    fun part2(input: String): Int {
+    fun solvePuzzle(input: String, i: Int): Int {
         var marker = ""
         var count = 0
         for (c in input) {
             if (c in marker) {
                 val index = marker.indexOf(c)
-                marker = marker.substring(index+1, marker.length) + c
+                marker = marker.substring(index + 1, marker.length) + c
             } else {
                 marker += c
             }
             count++
-            if (marker.length == 14) {
+            if (marker.length == i) {
                 break
             }
         }
         println(marker)
         return count
+    }
+
+    fun part1(input: String): Int {
+        return solvePuzzle(input, 4)
+    }
+
+    fun part2(input: String): Int {
+        return solvePuzzle(input, 14)
     }
 
     val testInput = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
