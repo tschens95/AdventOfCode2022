562d26f905da680b365df73368fbb7760b3b50ba
ser_pfote <jens@jheinen.de>
2022-12-07 23:08:02 +0100

    day07

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -78,30 +78,30 @@ fun main() {
         return mapDirectoryToSize.find { it >= requiredFreeSpace } ?: throw RuntimeException("ERROR 3")
     }
 
-    val testInput =
-            "\$ cd /\n" +
-            "\$ ls\n" +
-            "dir a\n" +
-            "14848514 b.txt\n" +
-            "8504156 c.dat\n" +
-            "dir d\n" +
-            "\$ cd a\n" +
-            "\$ ls\n" +
-            "dir e\n" +
-            "29116 f\n" +
-            "2557 g\n" +
-            "62596 h.lst\n" +
-            "\$ cd e\n" +
-            "\$ ls\n" +
-            "584 i\n" +
-            "\$ cd ..\n" +
-            "\$ cd ..\n" +
-            "\$ cd d\n" +
-            "\$ ls\n" +
-            "4060174 j\n" +
-            "8033020 d.log\n" +
-            "5626152 d.ext\n" +
-            "7214296 k"
+//    val testInput =
+//            "\$ cd /\n" +
+//            "\$ ls\n" +
+//            "dir a\n" +
+//            "14848514 b.txt\n" +
+//            "8504156 c.dat\n" +
+//            "dir d\n" +
+//            "\$ cd a\n" +
+//            "\$ ls\n" +
+//            "dir e\n" +
+//            "29116 f\n" +
+//            "2557 g\n" +
+//            "62596 h.lst\n" +
+//            "\$ cd e\n" +
+//            "\$ ls\n" +
+//            "584 i\n" +
+//            "\$ cd ..\n" +
+//            "\$ cd ..\n" +
+//            "\$ cd d\n" +
+//            "\$ ls\n" +
+//            "4060174 j\n" +
+//            "8033020 d.log\n" +
+//            "5626152 d.ext\n" +
+//            "7214296 k"
 
 //    println(part1(testInput.split("\n")))
 //    println(part2(testInput.split("\n")))
