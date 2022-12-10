13102f848d144aba979204fd2e2420b156366a64
ser_pfote <jens@jheinen.de>
2022-12-10 20:48:40 +0100

    day09 part1

--- src/day09/Day09.kt
+++ src/day09/Day09.kt
@@ -1,81 +1,61 @@
 package day09
 
+import readInput
 import kotlin.math.abs
 
 fun main() {
 
-    fun part1(input: List<String>) : Int {
-        val positions : MutableSet<Pair<Int, Int>> = mutableSetOf()
+    fun adjacent(h: Pair<Int, Int>, t: Pair<Int, Int>): Boolean {
+        val xDiff = abs(h.first - t.first)
+        val yDiff = abs(h.second - t.second)
+
+        return (xDiff == 0 && yDiff == 0) || (xDiff == 0 && yDiff == 1) || (xDiff == 1 && yDiff == 0)
+    }
+
+    fun diagonal(h: Pair<Int, Int>, t: Pair<Int, Int>): Boolean {
+        val xDiff = abs(h.first - t.first)
+        val yDiff = abs(h.second - t.second)
+        return (xDiff == 1 && yDiff == 1)
+    }
+
+    fun part1(input: List<String>): Int {
+        val positions: MutableSet<Pair<Int, Int>> = mutableSetOf()
         var posH = (0 to 0)
         var posT = posH
         positions.add(posT)
         val commands = input.map { Command(it.split(" ")[0], it.split(" ")[1].toInt()) }
         commands.forEach {
             when (it.direction) {
-                "R" ->  for (i in 0 until it.value) {
-                    // if not adjacent and on different y level, H above T
-                    if (abs(posH.first - posT.first) < 1 && abs (posH.second - posT.second) < 1) {
-                        posH = posH.copy(first = posH.first + 1)
-                    } else {
-                        posH = posH.copy(first = posH.first + 1)
-                        posT = if (abs(posH.first - posT.first) > 1 && posH.second - posT.second > 0) {
-                            posT.copy(posT.first + 1, posT.second + 1)
-                        } else if (abs(posH.first - posT.first) > 1 && posH.second - posT.second < 0) {
-                            posT.copy(posT.first + 1, posT.second - 1)
-                        } else {
-                            posT.copy(posT.first + 1)
-                        }
+                "R" -> for (i in 0 until it.value) {
+                    posH = posH.copy(first = posH.first + 1)
+                    if (!adjacent(posH, posT) && !diagonal(posH, posT)) {
+                        posT = posT.copy(posT.first + 1, posH.second)
                         positions.add(posT)
                     }
                 }
                 "L" -> for (i in 0 until it.value) {
-                    if (abs(posH.first - posT.first) < 1 && abs (posH.second - posT.second) < 1) {
-                        posH = posH.copy(first = posH.first - 1)
-                    } else {
-                        posH = posH.copy(first = posH.first - 1)
-                        posT = if (abs(posH.first - posT.first) > 1 && posH.second - posT.second > 0) {
-                            posT.copy(posT.first - 1, posT.second + 1)
-                        } else if (abs(posH.first - posT.first) > 1 && posH.second - posT.second < 0) {
-                            posT.copy(posT.first - 1, posT.second - 1)
-                        } else {
-                            posT.copy(posT.first - 1)
-                        }
+                    posH = posH.copy(first = posH.first - 1)
+                    if (!adjacent(posH, posT) && !diagonal(posH, posT)) {
+                        posT = posT.copy(posT.first - 1, posH.second)
                         positions.add(posT)
                     }
                 }
                 "U" -> for (i in 0 until it.value) {
-                    if (abs(posH.first - posT.first) < 1 && abs (posH.second - posT.second) < 1) {
-                        posH = posH.copy(second = posH.second + 1)
-                    } else {
-                        posH = posH.copy(second = posH.second + 1)
-                        posT = if (posH.first - posT.first > 0 && abs(posH.second - posT.second) > 1) {
-                            posT.copy(posT.first + 1, posT.second + 1)
-                        } else if (posH.first - posT.first < 0 && abs(posH.second - posT.second) > 1) {
-                            posT.copy(posT.first - 1, posT.second + 1)
-                        } else {
-                            posT.copy(posT.second + 1)
-                        }
+                    posH = posH.copy(second = posH.second + 1)
+                    if (!adjacent(posH, posT) && !diagonal(posH, posT)) {
+                        posT = posT.copy(posH.first, posT.second + 1)
                         positions.add(posT)
                     }
                 }
                 "D" -> for (i in 0 until it.value) {
-                    if (abs(posH.first - posT.first) < 1 && abs (posH.second - posT.second) < 1) {
-                        posH = posH.copy(second = posH.second - 1)
-                    } else {
-                        posH = posH.copy(second = posH.second - 1)
-                        posT = if (posH.first - posT.first > 0 && abs(posH.second - posT.second) > 1) {
-                            posT.copy(posT.first + 1, posT.second - 1)
-                        } else if (posH.first - posT.first < 0 && abs(posH.second - posT.second) > 1) {
-                            posT.copy(posT.first - 1, posT.second - 1)
-                        } else {
-                            posT.copy(posT.second + 1)
-                        }
+                    posH = posH.copy(second = posH.second - 1)
+                    if (!adjacent(posH, posT) && !diagonal(posH, posT)) {
+                        posT = posT.copy(posH.first, posT.second - 1)
                         positions.add(posT)
                     }
                 }
             }
         }
-        positions.also { print(it) }
         return positions.size
     }
 
@@ -89,4 +69,5 @@ fun main() {
             "R 2"
 
     println(part1(testInput.split("\n")))
+    println(part1(readInput("09")))
 }
